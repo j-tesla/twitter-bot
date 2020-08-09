d1d79e55b75ec8a3241359ccfcf2ca8d9a9083ea
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2020-08-09 23:00:28 +0530

    :sparkles: Added main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import json
+from botframe import BotFrame
+
+
+def main():
+    with open('keys.json', 'r') as f:
+        keys = json.load(f)
+
+    root = Tk()
+    root.title('Twitter bot')
+
+    gui = BotFrame(root, keys, padding="3 3 12 12")
+    gui.pack()
+    gui.mainloop()
+
+
+if __name__ == '__main__':
+    main()
